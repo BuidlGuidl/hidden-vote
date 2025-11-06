df5bf2db049224705c6cb45e7f509aa8c01d0924
phipsae <philip.krause88@yahoo.de>
2025-11-06 19:41:48 +0100

    add scrollbale feature

--- packages/nextjs/app/_components/OwnedVotings.tsx
+++ packages/nextjs/app/_components/OwnedVotings.tsx
@@ -133,7 +133,7 @@ const OwnedVotings = () => {
   }
 
   return (
-    <div className="w-full">
+    <div className="w-full max-h-[1100px] overflow-y-auto pr-2">
       <ul className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {votings.length === 0 ? (
           <li className="col-span-1 md:col-span-2 lg:col-span-3 bg-base-100 rounded-xl p-6 text-center opacity-70">
